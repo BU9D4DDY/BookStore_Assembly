0ba348acc67f6aeb6050b04419522a7ca86769b0
Abdullah <abdhamdy0909@gmail.com>
2022-01-04 19:25:56 +0200

    update data

--- src/main.asm
+++ src/main.asm
@@ -7,18 +7,18 @@
 ;;                                                 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-NewLine DB 0Dh,0Ah,"$"
+NewLine DB 13,10,"$"
                                                                                                                                                                              
-Logo1: DB "888888b.                     888        .d8888b.  888                            ",0,13,                                                                   
-       DB '888  "88b                    888       d88P  Y88b 888                            ',0,13,                                                                   
-       DB "888  .88P                    888       Y88b.      888                            ",0,13,                                                                    
-       DB '8888888K.   .d88b.   .d88b.  888  888   "Y888b.   888888 .d88b.  888d888 .d88b.  ',0,13,                                                                   
-       DB '888  "Y88b d88""88b d88""88b 888 .88P      "Y88b. 888   d88""88b 888P"  d8P  Y8b ',0,13,    
-       DB '888    888 888  888 888  888 888888K         "888 888   888  888 888    88888888 ',0,13,  
+Logo1: DB "888888b.                     888        .d8888b.  888                            ",13,                                                                   
+       DB '888  "88b                    888       d88P  Y88b 888                            ',13,                                                                   
+       DB "888  .88P                    888       Y88b.      888                            ",13,                                                                    
+       DB '8888888K.   .d88b.   .d88b.  888  888   "Y888b.   888888 .d88b.  888d888 .d88b.  ',13,                                                                   
+       DB '888  "Y88b d88""88b d88""88b 888 .88P      "Y88b. 888   d88""88b 888P"  d8P  Y8b ',13,    
+       DB '888    888 888  888 888  888 888888K         "888 888   888  888 888    88888888 ',13,  
        DB '888   d88P Y88..88P Y88..88P 888 "88b  Y88b  d88P Y88b. Y88..88P 888    Y8b. ',10,13,
        DB '8888888P"   "Y88P"   "Y88P"  888  888   "Y8888P"   "Y888 "Y88P"  888     "Y8888 ',10,13,"$"
-
-
+                                                                                                                                                                             
+                                                                                                                                                                             
 Logo2: DB "                              +-+-+-+-+-+-+-+",10,13,
        DB "                              |W|E|L|C|O|M|E|",10,13,
        DB "                              +-+-+-+-+-+-+-+",10,13,
@@ -29,7 +29,7 @@ GoodBye:
        DB "                              +-+-+-+-+-+-+-+",10,13,
        DB "                              |G|O|O|D|B|Y|E|",10,13,
        DB "                              +-+-+-+-+-+-+-+",10,13,
-       DB "                                   ",3,3,3,3,3,10,13,"$"
+       DB "                                   ",3,3,3,3,3,10,13,"$"                                                                                                                                                                             
 
 menu:  DB "   1- Get a membership",0Dh,0Ah
        DB "   2- Check Prices and Special offers",0Dh,0Ah 
@@ -38,6 +38,8 @@ menu:  DB "   1- Get a membership",0Dh,0Ah
        DB 10,13,"   Please Enter A Choice Between ( 1 - 4 ) ..... ",'$'
  
 wrong_choice DB 10,13,"   Please Enter A Choice Between ( 1 - 4 ) ..... ",'$'
+
+wrong_choice2 DB 10,13,"   Wrong Choice Please Enter A Choice Between ( 1 - 6 ) ..... ",'$'
 
 Total_msg: 
        DB 10,13,
@@ -48,30 +50,31 @@ Total_msg:
                                                                                                                                                                                                                                                                                   
 membership_menu:   
        DB 10,13,10,13,
-       DB "        Membership                         Price     Discount",0Dh,0Ah,0Dh,0Ah
-       DB "   1- Weekly membership        Adult     30 U.S.D       0%",0Dh,0Ah
-       DB "   2- Weekly membership        Child     15 U.S.D       0%",0Dh,0Ah
-       DB "   3- Monthly membership       Adult     96 U.S.D      20%",0Dh,0Ah
-       DB "   4- Monthly membership       Child     48 U.S.D      20%",0Dh,0Ah
-       DB "   5- Annuall membership       Adult    860 U.S.D      40%",0Dh,0Ah,
-       DB "   6- Annuall membership       Child    430 U.S.D      40%",0Dh,0Ah,0Dh,0Ah
-       DB "   Enter the number of the membership you want:  ",'$'
+       DB "        Membership                         Price",0Dh,0Ah,0Dh,0Ah
+       DB "   1- Weekly membership        Adult     20 U.S.D",0Dh,0Ah
+       DB "   2- Weekly membership        Child     10 U.S.D",0Dh,0Ah
+       DB "   3- Monthly membership       Adult     35 U.S.D",0Dh,0Ah
+       DB "   4- Monthly membership       Child     20 U.S.D",0Dh,0Ah
+       DB "   5- Annual membership        Adult     99 U.S.D",0Dh,0Ah,
+       DB "   6- Annual membership        Child     80 U.S.D",0Dh,0Ah,0Dh,0Ah
+       DB "   Enter the Type of the membership you want:  ",'$'
                                                                                                                                                                              
 membership_amount: 
        DB 0Dh,0Ah,"   How many membership do you want to buy (00-99)? ",'$'
 
 INFO:  DB 10,13,10,13,
-       DB "        BOOK NAME                          Price  ",0Dh,0Ah,0Dh,0Ah
-       DB "   In Search of Lost Time               300 U.S.D",0Dh,0Ah,
-       DB "   Don Quixote                          700 U.S.D",0Dh,0Ah,
-       DB "   The Great Gatsby                     450 U.S.D",0Dh,0Ah,
-       DB "   Moby Dick                            300 U.S.D",0Dh,0Ah,
-       DB "   To Kill a Mockingbird                320 U.S.D",0Dh,0Ah,
-       DB "   Things Fall Apart                    100 U.S.D",0Dh,0Ah,
-       DB "   The Color Purple                     250 U.S.D",0Dh,0Ah,
-       DB "   Don Quixote                          380 U.S.D",0Dh,0Ah,
-       DB "   The Call of the Wild                 690 U.S.D",0Dh,0Ah,
-       DB "   A Passage to India                   520 U.S.D",0Dh,0Ah,0Dh,0Ah
+       
+       DB "       BOOK NAME                          Price  ",0Dh,0Ah,0Dh,0Ah
+       DB "   Things Fall Apart                   100 U.S.D",0Dh,0Ah,       
+       DB "   Moby Dick                           250 U.S.D",0Dh,0Ah,
+       DB "   A Passage to India                  300 U.S.D",0Dh,0Ah,
+       DB "   To Kill a Mockingbird               300 U.S.D",0Dh,0Ah,
+       DB "   In Search of Lost Time              320 U.S.D",0Dh,0Ah,
+       DB "   The Color Purple                    380 U.S.D",0Dh,0Ah,
+       DB "   War and Peace                       450 U.S.D",0Dh,0Ah,
+       DB "   The Great Gatsby                    520 U.S.D",0Dh,0Ah,
+       DB "   The Call of the Wild                690 U.S.D",0Dh,0Ah,
+       DB "   Don Quixote                         700 U.S.D",0Dh,0Ah,0Dh,0Ah
        DB "   You Can Purchase Any Of These Books In Our Official Store ",3,3,3,0Dh,0Ah,10,13,'$'
 
 AboutUs:
@@ -81,25 +84,27 @@ AboutUs:
        DB "   +-+-+-+-+-+-+-+",10,13,
        DB 0Dh,0Ah,
        DB "   We Are THE AMAZING Book Store",0Dh,0Ah
-       DB "   We have textbooks, novels, biographies, and short stories",0Dh,0Ah 
+       DB "   We have Textbooks, Novels, Biographies, and Short Stories",0Dh,0Ah 
        DB "   The Store opens every day from 10:00AM to 04:00PM.",0Dh,0Ah
        DB "   You Can Contanct Us Via (+2) 012345678910.",0Dh,0Ah,,0Dh,0Ah,'$'
-    
+         
 Return DB "   Do You Want To Go Back ?(y/n) $"
 
 
-     
-adult    DD 30     ; adult Price 30 U.S.D
-child    DD 15     ; child Price 15 U.S.D
-adult20  DD 24     ; adult Price with 20% discount 24
-child20  DD 12     ; child Price with 20% discount 12
-adult40  DD 18     ; adult Price with 40% discount 18
-child40  DD 9      ; child Price with 40% discount 9
+
+adult    DB 20      ; WEEKLY Adult Price 20 U.S.D  
+child    DB 10      ; WEEKLY hild Price 10 U.S.D
+adultM   DB 35      ; Monthly Adult subscription
+childM   DB 20      ; Monthly Child subscription
+adultY   DB 99      ; Annual Adult subscription
+childY   DB 80      ; Annual Child subscription
+
 
 membership_type  DB 0
-membership_num   DD 0
+membership_num   DB 0
 result           DD 0
-RES          DB 10 DUP ('$')                                                                                                                                                                           
+RES              DB 5 DUP ('$')    ;Declare all as end of string for recognition in printing
+                                                                                                                                                                          
 
                      
 .code
